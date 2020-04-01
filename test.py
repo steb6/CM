b512045924ea4f09caffbd63fd24d255745b8afa
StefanoBerti <berti.ste@hotmail.it>
2020-04-01 14:59:05 +0200

    Added solution to QR

--- test.py
+++ test.py
@@ -8,17 +8,23 @@ from conjugate_gradient import conjugate_gradient
 functions = [qr_factorization1, qr_factorization2, qr_factorization3]
 
 A, b = read_data('data/ML-CUP19-TR.csv')
-
-x = conjugate_gradient(A, b)
+m, n = A.shape
+# x = conjugate_gradient(A, b)
 
 for i, qr_factorization in enumerate(functions):
     start = time.monotonic_ns()
     Q, R = qr_factorization(A)
     done = time.monotonic_ns()
     elapsed = done - start
+    # Calcolo soluzione
+    R1 = R[:n, :n]
+    Q1 = Q[:, :n]
+    Q1T = np.transpose(Q1)
+    x = np.matmul(np.linalg.inv(R1), np.matmul(Q1T, b))
     # R = np.around(R, decimals=6)
     print("QR Factorization n° {} ended".format(i+1))
     print("ns spent: ", elapsed)
-    print("||A - QR|| =", norm(A - np.matmul(Q, R)))
+    print("||A - QR|| =", norm(A - np.matmul(Q1, R1)))
+    print("||Ax - b|| =", norm(np.matmul(A, x) - b))
 
 

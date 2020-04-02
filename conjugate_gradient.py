780f190239c7cae21b5d51947983f9995477a390
StefanoBerti <berti.ste@hotmail.it>
2020-04-02 15:49:08 +0200

    Added positive definite checking

--- conjugate_gradient.py
+++ conjugate_gradient.py
@@ -2,7 +2,19 @@ import numpy as np
 from utils import transpose_matrix
 
 
+def is_pos_def(x):
+    return np.all(np.linalg.eigvals(x) > 0) and is_symmetric(x)
+
+
+def is_symmetric(a, rtol=1e-05, atol=1e-08):
+    return np.allclose(a, a.T, rtol=rtol, atol=atol)
+
+
 def conjugate_gradient(A, b):
+    if is_pos_def(np.matmul(transpose_matrix(A), A)):
+        print("A_T*A is positive definite")
+    else:
+        print("A_T*A is not positive definite")
     x = 0
     r = d = b
     for k in range(len(A)):

4d7582eb690d699bdbc106dd1df9f992d593d2a9
StefanoBerti <berti.ste@hotmail.it>
2020-04-02 16:13:57 +0200

    Added conditioning angle control

--- test_qr.py
+++ test_qr.py
@@ -11,6 +11,15 @@ functions = [np.linalg.qr, qr_factorization1, qr_factorization2, qr_factorizatio
 A, b = read_data('data/ML-CUP19-TR.csv')
 m, n = A.shape
 
+
+def conditioning_angle(a_, b_, x_):
+    '''
+    if this value is little, we know that the angle between b and Ax is little and so b is close to the ImA, and
+    the problem is well conditioned
+    '''
+    return np.arccos(np.divide(norm(np.matmul(a_, x_)), norm(b_)))
+
+
 for i, qr_factorization in enumerate(functions):
     print("**********************************************")
     print(qr_factorization.__name__)
@@ -30,6 +39,7 @@ for i, qr_factorization in enumerate(functions):
     print("||A - QR||/||A||", np.divide(norm(A - np.matmul(Q1, R1)), norm(A)))
     print("||Ax - b|| =", norm(np.matmul(A, x) - b))
     print("||Ax - b||/||b|| =", np.divide(norm(np.matmul(A, x) - b), norm(b)))
+    print("Conditioning angle: ", conditioning_angle(A, b, x))
 
 # Check if computational cost scale with m
 print("**********************************************************")

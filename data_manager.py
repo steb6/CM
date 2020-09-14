faef330f355aef312aa234f50f1ca143273ae1de
StefanoBerti <berti.ste@hotmail.it>
2020-09-14 16:44:49 +0200

    Updated data_manager, added requirements.txt

--- data_manager.py
+++ data_manager.py
@@ -8,23 +8,20 @@ def read_data(filename):
 
     # Print dimensions
     print('Readed {} rows and {} column'.format(A.shape[0], A.shape[1]))
-    #print('First line: '+str(A[0]))
 
     # Remove indexes
-    A = np.delete(A, 0, 1)
-    y = A.shape[1]
-    b = A[:, y-1]
-    A = np.delete(A, np.s_[y-2, y], axis=1)
-
-    # Print new dimensions
-    #print('Deleted indexes, new dimensions are {} rows and {} column'.format(A.shape[0], A.shape[1]))
-    #print('First line: '+str(A[0]))
+    A = A[:, 1:]
+    # Extract last 2 columns for b
+    b = A[:, -2:]
+    # But b must have a single dimension
+    b = b[:, 0]
+    # Remove last 2 columns from A
+    A = A[:, :-2]
 
     # Add derived columns
     A_1 = np.log(np.abs(A[:, 0]))
     A_2 = A[:, 1] * A[:, 2] * A[:, 3]
     A_3 = np.square(A[:, 4])
     A = np.c_[A, A_1, A_2, A_3]
-    #print('Added new columns, new dimensions are {} rows and {} column'.format(A.shape[0], A.shape[1]))
-    #print('First line: '+str(A[0]))
+
     return A, b

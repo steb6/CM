422323fed209bcb3f8239819017e9726f2fbf149
StefanoBerti <berti.ste@hotmail.it>
2020-09-22 12:19:14 +0200

    Fixed little things

--- qr_householder.py
+++ qr_householder.py
@@ -8,11 +8,11 @@ def householder_vector(x):
     :param x: a column vector
     :return: a column vector useful to form H
     '''
-    norm_x = -np.sign(x[0]) * norm(x)
-    vec = np.copy(x)
-    vec[0] = vec[0] - norm_x
-    vec = np.true_divide(vec, norm(vec))
-    return vec, norm_x
+    s = -np.sign(x[0]) * norm(x)
+    v = np.copy(x)
+    v[0] = v[0] - s
+    v = np.true_divide(v, norm(v))
+    return v, s
 
 
 def qr_factorization1(x_):
@@ -68,17 +68,15 @@ def qr_factorization3(x_):
     '''
     x_ = np.copy(x_)
     [m, n] = x_.shape
-    Q_ = np.identity(m)
-    V =[]
+    V = []
     for j in range(min(m-1, n)):
-        col = row_to_column(x_[j:, j])
-        v_, s_ = householder_vector(col) # v_ is a column vector (3, 1)
-        v_T = column_to_row(v_) # v_T is a row vector (1, 3)
+        v_, s_ = householder_vector(row_to_column(x_[j:, j]))
         x_[j, j] = s_
         x_[j+1:, j] = 0
-        x_[j:, j+1:] = x_[j:, j+1:] - 2 * np.matmul(v_, np.matmul(v_T, x_[j:, j+1:]))
+        x_[j:, j+1:] = x_[j:, j+1:] - 2 * np.matmul(v_, np.matmul(column_to_row(v_), x_[j:, j+1:]))
         V.append(v_)
 
+    Q_ = np.identity(m)
     for j, v_ in enumerate(V):
         Q_[:, j:] = Q_[:, j:] - np.matmul(Q_[:, j:], 2*np.matmul(v_, column_to_row(v_)))
     R_ = x_
--- test_qr.py
+++ test_qr.py
@@ -1,12 +1,16 @@
 from qr_householder import qr_factorization1, qr_factorization2, qr_factorization3
 import numpy as np
-#from utils import norm
+# from utils import norm
 from numpy.linalg import norm
 from data_manager import read_data
 import time
 import matplotlib.pyplot as plt
+import tqdm
+from numpy.linalg import lstsq
+from numpy.linalg import qr
 
-functions = [np.linalg.qr, qr_factorization1, qr_factorization2, qr_factorization3]
+functions = [np.linalg.qr, qr_factorization3]
+# functions = [qr_factorization3]
 
 A, b = read_data('data/ML-CUP19-TR.csv')
 m, n = A.shape
@@ -46,25 +50,25 @@ for i, qr_factorization in enumerate(functions):
     #btilde = b + np.random.randint(-np.finfo(float).eps, np.finfo(float), b.shape, float)
 
 # Check if computational cost scale with m
-print("**********************************************************")
-print("Factorizing for various m...")
-times = []
-sizes = range(n, m, 100)
-for k in sizes:
-    print(k/m)
-    A_ = A[:k, :]
-    start = time.monotonic_ns()
-    Q, R = qr_factorization(A_)
-    done = time.monotonic_ns()
-    elapsed = done - start
-    times.append(elapsed)
+#print("**********************************************************")
+#print("Factorizing for various m...")
+#times = []
+#sizes = range(n, m, 100)
+#for k in sizes:
+#    print(k/m)
+#    A_ = A[:k, :]
+#    start = time.monotonic_ns()
+#    Q, R = qr_factorization(A_)
+#    done = time.monotonic_ns()
+#    elapsed = done - start
+#    times.append(elapsed)
 
 # Creating plot
-print("Creating plot...")
-plt.plot(sizes, times)
-plt.ylabel("Time for QR factorization")
-plt.xlabel("Largest dimension of A")
-plt.show()
+#print("Creating plot...")
+#plt.plot(sizes, times)
+#plt.ylabel("Time for QR factorization")
+#plt.xlabel("Largest dimension of A")
+#plt.show()
 
 exit()
 
@@ -72,11 +76,13 @@ exit()
 print("**********************************************************")
 print("m>>n: Factorizing for various m...")
 times = []
-n = 10
-m = 10000
-sizes = range(n, m, 1000)
-for k in sizes:
-    print(k/m)
+n = 5
+m_init = 2000
+m_end = 8000
+m_step = 250
+sizes = range(m_init, m_end, m_step)
+for k in tqdm.tqdm(sizes):
+    #print("Biggest dimension is "+str(k))
     A_ = np.random.rand(k, n)
     start = time.monotonic_ns()
     Q, R = qr_factorization3(A_)
@@ -86,7 +92,14 @@ for k in sizes:
 
 # Creating plot
 print("Creating plot...")
-plt.plot(sizes, times)
+times = [elem/1000000 for elem in times]
+x = np.array(sizes)
+y = np.array(times)
+A = np.vstack([x, np.ones(len(x))]).T
+m, c = lstsq(A, y, rcond=None)[0]
+plt.plot(x, m*x + c, 'r', label='Fitted line')
+plt.plot(sizes, times, label='Times')
+plt.legend()
 plt.ylabel("Time for QR factorization")
 plt.xlabel("Largest dimension of A")
 plt.show()

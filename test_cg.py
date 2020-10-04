fe054ecbc6934febeb5218b266b9d63eb4a3ffaa
AlessandroPoggiali <56312789+AlessandroPoggiali@users.noreply.github.com>
2020-10-04 11:19:12 +0200

    Update test_cg.py

--- test_cg.py
+++ test_cg.py
@@ -4,12 +4,16 @@ import numpy as np
 from numpy.linalg import norm
 from data_manager import read_data
 import time
-from conjugate_gradient import conjugate_gradient, conjugate_gradient1
+from conjugate_gradient import conjugate_gradient1, conjugate_gradient2
 from scipy.sparse.linalg import cg
 
 functions = [qr_factorization1, qr_factorization2, qr_factorization3]
 
 A, b = read_data('data/ML-CUP19-TR.csv')
 m, n = A.shape
-x = conjugate_gradient1(A, b)
-print("CG: ||Ax - b|| =", norm(np.matmul(A, x) - b))
+x1 = conjugate_gradient1(A, b)
+x2 = conjugate_gradient2(A, b)
+xnp = np.linalg.lstsq(A, b)
+print("Numpy least squares: ||Ax - b|| =", norm(np.matmul(A, xnp[0]) - b))
+print("CG1: ||Ax - b|| =", norm(np.matmul(A, x1) - b))
+print("CG2: ||Ax - b|| =", norm(np.matmul(A, x2) - b))

49a91628142283172ddb7d19fba5f93a36ecdfbd
AlessandroPoggiali <56312789+AlessandroPoggiali@users.noreply.github.com>
2020-10-04 11:18:40 +0200

    Update conjugate_gradient.py

--- conjugate_gradient.py
+++ conjugate_gradient.py
@@ -30,50 +30,43 @@ def conjugate_gradient(A, b):
     return x
 '''
 
-# TRY WITH DIFFERENT FORMULAs FOR BETA
 
 # algorithm from https://www.stat.washington.edu/wxs/Stat538-w03/conjugate-gradients.pdf
 def conjugate_gradient1(A, b):
-    if is_pos_def(np.matmul(transpose_matrix(A), A)):
-        print("A_T*A is positive definite")
-    else:
-        print("A_T*A is not positive definite")
     x = np.zeros(A.shape[1])
     r =  b  - np.matmul(A, x)
     g_0 = np.matmul(transpose_matrix(A), r)
+    d = g_0
     g = g_1 = g_0
     for i in range(1, A.shape[1]):
-        if i>1:
-            g_2 = g_1
-            g_1 = g
         if not np.any(g):
             return x
         if i>1:
+            g_2 = g_1
+            g_1 = g
             beta = -np.divide(np.square(norm(g_1)),np.square(norm(g_2)))
-        if i==1: 
-            p = g_0
-        else:
-            p = g_1 - beta*p
-        alpha = np.divide(np.square(norm(g_1)),np.square(norm(np.matmul(A, p))))
-        x = x + alpha*p
-        r = r - alpha*np.matmul(A,p)
+            d = g_1 - beta*d   
+        Ad = np.matmul(A, d) 
+        alpha = np.divide(np.square(norm(g_1)),np.square(norm(Ad)))
+        x = x + alpha*d
+        r = r - alpha*Ad
         g = np.matmul(transpose_matrix(A), r)
-    return x        
+    return x               
 
-# algorithm from https://math.aalto.fi/opetus/inv/CGalgorithm.pdf
+# algorithm from https://nvlpubs.nist.gov/nistpubs/jres/049/jresv49n6p409_A1b.pdf
 def conjugate_gradient2(A, b):
     x = np.zeros(A.shape[1])
-    d = b
-    r = np.matmul(np.transpose(A),b)
-    p = r
-    t = np.matmul(A,p)
-    for k in range(1, A.shape[1]):
-        alpha = np.divide(np.square(norm(r)), np.square(norm(t)))
-        x = x + alpha*p
-        d = d - alpha*t
-        r_1 = r
-        r = np.matmul(np.transpose(A),d)
-        beta = np.divide(np.square(norm(r)),np.square(norm(r_1)))
-        p = r + beta*p
-        t = np.matmul(A, p)
+    r =  b  - np.matmul(A, x)
+    #g_0 = np.matmul(transpose_matrix(A), r)
+    d = np.matmul(transpose_matrix(A), r)
+    #g = g_1 = g_0
+    for i in range(1, A.shape[1]):
+        alpha = np.divide(np.square(norm(np.matmul(transpose_matrix(A), r))),np.square(norm(np.matmul(A, d))))
+        x = x + alpha*d
+        r_ = r
+        r = r - alpha*np.matmul(A,d)
+        beta = np.divide(np.square(norm(np.matmul(transpose_matrix(A), r))),np.square(norm(np.matmul(transpose_matrix(A), r_))))
+        d = np.matmul(transpose_matrix(A),r) + beta*d
     return x
+
+
